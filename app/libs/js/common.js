aab02c15709430afe81b9ccb8623063c8e266e7e
rydolif <rudolifrudolif@gmail.com>
2018-05-24 19:11:34 +0300

    finish

---
+++ app/libs/js/common.js
@@ -0,0 +1,52 @@
+$(function() {
+
+//----------menu-------------
+	$("#my-menu").mmenu({
+		"extensions": [
+			 "position-right",
+			 "fx-panels-zoom",
+			 "position-back",
+			 "fx-listitems-slide"
+		],
+		navbar: {
+			title: '<img src="img/footer-svg.svg" alt="list" class="header__list-svg">Our offices:'
+		}
+	},{clone:true});
+
+//--------перестало працювати---------
+	// var api = $("#my-menu").data( "mmenu" );
+	// api.bind('openPanel:before', function() {
+	// 	$('.hamburger').addClass('is-active');
+	// });
+	// api.bind('closePanel:before', function() {
+	// 	$('.hamburger').removeClass('is-active');
+	// });
+//----------menu-------------
+
+//---------якоря-------------
+	$("#wrapper").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+					top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 500);
+	});
+//---------якоря-------------
+
+
+//-------tabs----------
+	$('.tab-3__wrap').hide();
+  $('.tab-3__wrap:first').show();
+  $('.tabs li:first').addClass('main__item-active');
+
+  $('.tabs li').click(function(event){
+    $('.tabs li').removeClass('main__item-active');
+    $(this).addClass('main__item-active');
+    $('.tab-3__wrap').hide();
+
+    var selectTab = $(this).find('a').attr('href');
+    $(selectTab).fadeIn();
+  });
+//-------tabs----------
+
+
+});
